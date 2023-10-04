f2efb8b557ee08096fbc17736ab70b1ee6670e81
briskt <3172830+briskt@users.noreply.github.com>
2023-10-04 16:17:56 -0600

    use the full image URL

--- imagescan.sh
+++ imagescan.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 
-dfctl scan -f json -O results deepfactor-experiment:develop
+dfctl scan -f json -O results 369020531563.dkr.ecr.us-east-1.amazonaws.com/deepfactor-experiment:develop
